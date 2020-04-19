eec822a4802522007e7c36ad420ba2493e128b75
dporr <dporras.mail@gmail.com>
2020-04-19 02:37:25 -0700

    fixed typo

--- deploy_cluster.sh
+++ deploy_cluster.sh
@@ -1,3 +1,3 @@
 echo "Running cluster creation based on: cluster.yml...\n\n"
 echo "Saving output to: cluster_creation.txt"
-eksctl create cluster -f cluster.yaml|tee cluster_creation.txt
+eksctl create cluster -f cluster.yml|tee cluster_creation.txt
